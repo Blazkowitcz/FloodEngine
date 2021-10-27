3e81bc09d3b9d064ab4149c8d1e009a420fff1c0
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-10-27 18:48:56 +0200

    Upload a new torrent if not already in database

--- app/controllers/torrent.controller.js
+++ app/controllers/torrent.controller.js
@@ -8,11 +8,20 @@ const Torrent = require('../models/torrent.model');
  * @param {Result} res 
  */
 exports.upload = async (req, res) => {
-    let file = parse_torrent(req.files.torrent.data);
-    let torrent = new Torrent({name: file.name, hash: file.infoHash, user_id: 697});
-    let exist = await Torrent.checkIfTorrentExist(file.infoHash);
+    let data = parse_torrent(req.files.torrent.data);
+    let exist = await Torrent.checkIfTorrentExist(data.infoHash);
     if(!exist){
+        let file = req.files.torrent ;
+        let filename = crypto.randomBytes(16).toString("hex") + '.torrent';
+        let torrent = new Torrent({
+            name: data.name, 
+            filename: filename,
+            hash: data.infoHash, 
+            user_id: 697,
+            size: data.length
+        });
         await torrent.save();
+        file.mv('./public/torrents/' + filename);
     }
     res.send(true);
 }
