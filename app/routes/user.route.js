13c6edc06345c71c11e09b66395aa995898bc136
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-11-11 10:39:41 +0100

    Update User route

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -1,6 +1,9 @@
 module.exports = app => {
     var user = require('../controllers/user.controller');
 
+    /**
+     * POST Routes
+     */
     app.post('/signup', user.signup);
     app.post('/signin', user.signin);
 };
