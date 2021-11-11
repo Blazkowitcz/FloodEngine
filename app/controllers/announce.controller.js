28e169f089229f4b63179e3d76b9527c102b74e4
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-11-11 10:14:49 +0100

    Update announcer

--- app/controllers/announce.controller.js
+++ app/controllers/announce.controller.js
@@ -1,14 +1,28 @@
 const bencode = require('bencode');
+const Peer = require('../models/peer.model');
+const User = require('../models/user.model');
 
-exports.announce = (req, res) => {
-    //let hash = Buffer.from(req.query.info_hash, 'ascii').toString();
+/**
+ * Announcer
+ * @param {Request} req 
+ * @param {Result} res 
+ */
+exports.announce = async (req, res) => {
+    let user = await User.getUserByPasskey(req.params.passkey);
+    let peer = await Peer.findOne({hash: req.query.info_hash, user_id: user.id, ip: req.headers.host.substring(0, req.headers.host.indexOf(':')), port: req.query.port});
+    peer = peer !== null ? new Peer(peer) : new Peer({hash: req.query.info_hash, user_id: user.id});
+    peer.date = new Date();
+    peer.port = req.query.port;
+    peer.ip = req.headers.host.substring(0, req.headers.host.indexOf(':'));
+    peer.save();
+    let peers = await Peer.getPeersFromHash(req.query.info_hash);
     let data = {
         'interval' : 2700,
         'min_interval' : 1800,
-        'tracker_id' : '127.0.0.1:3000',
+        'tracker_id' : 'http://127.0.0.1:3000',
         'complete': 0,
         'incomplete': 0,
-        'peers': [{ip: "192.168.1.86", port: 7894}],
+        'peers': peers,
         'message': req.query.info_hash,
     };
     res.setHeader('Content-Type', 'text/plain');
