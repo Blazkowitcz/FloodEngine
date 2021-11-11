f1f492735c3afe6726111313f10cb4f0b373be4c
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-11-11 10:38:57 +0100

    Update Announce route

--- app/routes/announce.route.js
+++ app/routes/announce.route.js
@@ -1,5 +1,8 @@
 module.exports = app => {
     var announcer = require('../controllers/announce.controller');
 
+    /**
+     * GET Routes
+     */
     app.get('/announce/:passkey', announcer.announce);
 };
