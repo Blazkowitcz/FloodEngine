6fa24a1ac8f137a8ded81f22edee60c2045e3246
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-11-11 10:39:59 +0100

    Update Torrent utils tool

--- app/utils/torrent.util.js
+++ app/utils/torrent.util.js
@@ -17,6 +17,11 @@ function editTorrent(){
     });
 }
 
+/**
+ * Remove special char like long dash
+ * @param {String} name 
+ * @returns 
+ */
 function formatName(name){
     name = name.replace(/\u2013|\u2014/g, "-");
     return name;
