40e6709c73fdc35da1c7a50e4ff13fd2b5a6f64b
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-11-11 10:34:57 +0100

    Get user with passkey

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,11 +1,27 @@
 const Model = require('../modules/model.module');
 
-class User extends Model{
-    constructor(data){
+class User extends Model {
+    constructor(data) {
         super();
-        for(let key in data){
+        for (let key in data) {
             this[key] = data[key];
         }
+    }
+
+    /**
+     * Return user id 
+     * @param {String} passkey 
+     * @returns 
+     */
+    static async getUserByPasskey(passkey) {
+        let user = await this.__proto__.findOne({ passkey: passkey }, { table: this.name });
+        return new Promise(function (resolve, reject) {
+            if (user) {
+                resolve(user);
+            } else {
+                reject(null);
+            }
+        });
     }
 }
 
