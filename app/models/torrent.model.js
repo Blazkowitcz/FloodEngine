59860a5a62232e6bd3da679a39a6681362554ee3
Blazkowicz <bonnet.nicolas66@gmail.com>
2021-11-11 10:33:58 +0100

    Get last torrents and change torrent get request

--- app/models/torrent.model.js
+++ app/models/torrent.model.js
@@ -15,16 +15,32 @@ class Torrent extends Model {
      * @returns {Bool}
      */
     static async checkIfTorrentExist(hash) {
-        let torrent = await this.__proto__.findOne({ hash: hash }, this.name);
+        let torrent = await this.__proto__.findOne({ hash: hash }, {table: this.name});
         return new Promise(function (resolve, reject) {
             resolve(torrent !== null);
         });
     }
 
+    /**
+     * Return torrent related to id
+     * @param {Integer} id 
+     * @returns 
+     */
     static async getTorrentById(id) {
-        let torrent = await this.__proto__.findOne({ id: id }, this.name);
+        let torrent = await this.__proto__.findOne({ id: id }, {table: this.name, limit: 1, order: {column: 'created_at', direction: 'ASC'}});
         return new Promise(function (resolve, reject) {
             resolve(torrent);
+        });
+    }
+
+    /**
+     * Return 25 last torrents
+     * @returns 
+     */
+    static async getLastTorrents() {
+        let torrents = await this.__proto__.find(null, {table: this.name, limit: 25, order: {column: 'id', direction: 'DESC'}});
+        return new Promise(function (resolve, reject) {
+            resolve(torrents);
         });
     }
 }
